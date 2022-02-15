6c3e9a55295cb1b7891310a14434fed9fd07d673
Hana Wujira <hana.wujira@transputec.com>
2022-02-15 14:32:18 +0300

    Update Department.cs

--- CrisesControl/CrisesControl.Core/DepartmentAggregate/Department.cs
+++ CrisesControl/CrisesControl.Core/DepartmentAggregate/Department.cs
@@ -4,11 +4,7 @@ using System.Collections.Generic;
 
 namespace CrisesControl.Core.DepartmentAggregate
 {
-    public record Department(string Name, List<string> Pings): BaseEntity, IAggregateRoot
+    public record Department(string Name    ): BaseEntity, IAggregateRoot
     {
-        public void AddPing(string empty)
-        {
-            Pings.Add(empty);
-        }
     }
 }
