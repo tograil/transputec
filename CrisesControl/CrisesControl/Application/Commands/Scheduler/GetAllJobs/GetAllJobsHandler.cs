44c0dd2746aca914fcd3697aff480256f5ab8c16
Confidence Selomo <selomomc@gmail.com>
2022-05-03 11:12:07 +0200

    adding the validator on the GetAllJobsHandler

--- CrisesControl/CrisesControl/Application/Commands/Scheduler/GetAllJobs/GetAllJobsHandler.cs
+++ CrisesControl/CrisesControl/Application/Commands/Scheduler/GetAllJobs/GetAllJobsHandler.cs
@@ -1,11 +1,14 @@
-﻿using AutoMapper;
+﻿using Ardalis.GuardClauses;
+using AutoMapper;
 using CrisesControl.Api.Application.Query;
+using FluentValidation;
 using MediatR;
 
 namespace CrisesControl.Api.Application.Commands.Scheduler.GetAllJobs
 {
     public class GetAllJobsHandler : IRequestHandler<GetAllJobsRequest, GetAllJobsResponse>
     {
+        private readonly GetAllJobsValidator _exTriggertValidator;
         private readonly ISchedulerQuery _schedulerQuery;
         private readonly IMapper _mapper;
         public GetAllJobsHandler(ISchedulerQuery schedulerQuery, IMapper mapper)
@@ -15,6 +18,9 @@ namespace CrisesControl.Api.Application.Commands.Scheduler.GetAllJobs
         }
         public async Task<GetAllJobsResponse> Handle(GetAllJobsRequest request, CancellationToken cancellationToken)
         {
+            Guard.Against.Null(request, nameof(GetAllJobsRequest));
+
+            await _exTriggertValidator.ValidateAndThrowAsync(request, cancellationToken);
             var result = await _schedulerQuery.GetAllJobs(request);
             return result;
         }
