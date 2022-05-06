a249017aa3d051a3d39931a766f8275fb8342304
Confidence Selomo <selomomc@gmail.com>
2022-05-06 20:40:09 +0200

    used pascals

---
+++ CrisesControl/CrisesControl/Application/Commands/Scheduler/GetAllJobs/GetAllJobsValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace CrisesControl.Api.Application.Commands.Scheduler.GetAllJobs
+{
+    public class GetAllJobsValidator:AbstractValidator<GetAllJobsRequest>
+    {
+       public GetAllJobsValidator()
+       {
+            RuleFor(x => x.UserID).GreaterThan(0);
+            RuleFor(x => x.CompanyID).GreaterThan(0);
+        }
+    }
+}
