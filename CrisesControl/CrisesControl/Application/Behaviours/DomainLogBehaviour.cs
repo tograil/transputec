815c0ffe3d26e19fa0a8aeaba466bcaf2de3c178
Andriy Rubayko <andriy.rubayko@transputec.com>
2022-05-26 18:38:11 +0300

    Made audit log optional

--- CrisesControl/CrisesControl/Application/Behaviours/DomainLogBehaviour.cs
+++ CrisesControl/CrisesControl/Application/Behaviours/DomainLogBehaviour.cs
@@ -1,8 +1,10 @@
 ﻿using CrisesControl.Api.Application.Helpers;
+using CrisesControl.Config;
 using CrisesControl.Core.AuditLog;
 using CrisesControl.Core.AuditLog.Services;
 using MediatR;
 using MediatR.Pipeline;
+using Microsoft.Extensions.Options;
 
 namespace CrisesControl.Api.Application.Behaviours
 {
@@ -11,16 +13,21 @@ namespace CrisesControl.Api.Application.Behaviours
     {
         private readonly IAuditLogService _auditLogService;
         private readonly ICurrentUser _currentUser;
+        private readonly AuditLogOptions _options;
 
-        public DomainLogBehaviour(IAuditLogService auditLogService, ICurrentUser currentUser)
+        public DomainLogBehaviour(IAuditLogService auditLogService, ICurrentUser currentUser, IOptions<AuditLogOptions> options)
         {
             _auditLogService = auditLogService;
             _currentUser = currentUser;
+            _options = options.Value;
         }
 
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
             var response = await next();
+
+            if (!_options.Active)
+                return response;
 
             var savedData = _auditLogService.GetSaveChangesAudit();
             var userId = _currentUser.UserId;
--- CrisesControl/CrisesControl/Config/AuditLogOptions.cs
+++ CrisesControl/CrisesControl/Config/AuditLogOptions.cs
@@ -3,5 +3,6 @@
     public class AuditLogOptions
     {
         public string ServerAddress { get; set; }
+        public bool Active { get; set; }
     }
 }
