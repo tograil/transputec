3138f5cb3ae090b7658f1861145d729743154bae
Confidence Selomo <Confidence.Selomo@transputec.com>
2022-09-01 20:08:18 +0200

    UpdateSegregationLink

---
+++ CrisesControl/CrisesControl/Application/Commands/Departments/CheckDepartment/CheckDepartmentValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace CrisesControl.Api.Application.Commands.Departments.CheckDepartment
+{
+    public class CheckDepartmentValidator:AbstractValidator<CheckDepartmentRequest>
+    {
+        public CheckDepartmentValidator()
+        {
+            RuleFor(x => x.DepartmentId).GreaterThan(0);
+        }
+    }
+}
