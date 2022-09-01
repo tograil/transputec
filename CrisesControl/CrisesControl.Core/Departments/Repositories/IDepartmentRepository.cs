f7642f13d9504cf5e0136cc89e55668c783bac56
Confidence Selomo <Confidence.Selomo@transputec.com>
2022-09-01 12:07:50 +0200

    tested Development

--- CrisesControl/CrisesControl.Core/Departments/Repositories/IDepartmentRepository.cs
+++ CrisesControl/CrisesControl.Core/Departments/Repositories/IDepartmentRepository.cs
@@ -14,8 +14,8 @@ namespace CrisesControl.Core.Departments.Repositories
         Task<int> UpdateDepartment(Department department, CancellationToken token);
         Task<int> DeleteDepartment(int departmentId, CancellationToken token);
         bool CheckDuplicate(Department department);
-        bool CheckForExistance(int departmentId);
-        Task<bool> UpdateSegregationLink(int SourceID, int TargetID, string Action, GroupType LinkType, int CompanyId);
+        Task<bool> CheckForExistance(int departmentId);
+        Task<bool> UpdateSegregationLink(int SourceID, int TargetID,  GroupType LinkType, int CompanyId);
         Task<List<GroupLink>> SegregationLinks(int TargetID, string MemberShipType, string LinkType, int CurrentUserId, int OutUserCompanyId);
         Task<int> DepartmentStatus(int CompanyID);
         Task CreateSegregtionLink(int sourceID, int targetID, GroupType LinkType, int companyId);
---
+++ CrisesControl/CrisesControl/Application/Commands/Departments/GetDepartment/GetDepartmentHandler.cs
@@ -0,0 +1,29 @@
+﻿using Ardalis.GuardClauses;
+using CrisesControl.Api.Application.Query;
+using FluentValidation;
+using MediatR;
+
+namespace CrisesControl.Api.Application.Commands.Departments.GetDepartment
+{
+    public class GetDepartmentHandler : IRequestHandler<GetDepartmentRequest, GetDepartmentResponse>
+    {
+
+        private readonly ILogger<GetDepartmentHandler> _logger;
+        private readonly GetDepartmentValidator _departmentValidator;
+        private readonly IDepartmentQuery _departmentQuery;
+        public GetDepartmentHandler(GetDepartmentValidator departmentValidator, IDepartmentQuery departmentQuery, ILogger<GetDepartmentHandler> logger)
+        {
+            _departmentValidator = departmentValidator;
+            _departmentQuery = departmentQuery;
+            _logger = logger;
+        }
+        public async Task<GetDepartmentResponse> Handle(GetDepartmentRequest request, CancellationToken cancellationToken)
+        {
+            Guard.Against.Null(request, nameof(GetDepartmentRequest));
+
+            await _departmentValidator.ValidateAndThrowAsync(request, cancellationToken);
+            var result = await _departmentQuery.GetDepartment(request, cancellationToken);
+            return result;
+        }
+    }
+}
